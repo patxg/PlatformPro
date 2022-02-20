ca8de455c4fd513c3d872723f29c2b876cb28bf8
patxg <pat.x.guillen@gmail.com>
2022-02-20 02:03:37 -0500

    Update Platform

---
+++ PlatformPro/Assets/Scripts/Coins.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                player.AddCoins();
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ PlatformPro/Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Controller
+    private CharacterController _controller;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _gravity = 1.0f;
+    [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _yVelocity;
+    private bool _canDoubleJump = false;
+
+    //Coin
+    [SerializeField] private int _coins;
+    private UIManager _uiManager;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        if (_controller == null)
+            Debug.LogError("Character controller is NULL");
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        if (_uiManager == null)
+            Debug.LogError("UIManager is Null");
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, _yVelocity, 0);
+        Vector3 velocity = direction * _speed;
+
+        if (_controller.isGrounded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (_canDoubleJump == true)
+                {
+                    _yVelocity += _jumpHeight;
+                    _canDoubleJump = false;
+                }
+            }
+            _yVelocity -= _gravity;
+        }
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void AddCoins()
+    {
+        _coins++;
+        _uiManager.UpdateCoinDisplay(_coins);
+    }
+
+}
